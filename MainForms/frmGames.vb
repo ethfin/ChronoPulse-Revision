3caf2cfc08355ce0f25e0f9573a6cf5e63b94166
ethfin <christophergalano13@gmail.com>
2024-08-14 14:13:25 +0800

    Add app usage tracking with timer and ListView display
    Added `Imports System.Diagnostics` for process tracking. Introduced `trackingTimer`, `trackedApps`, and `totalElapsedTimes` in `frmGames` class. Configured `ListViewTrackApp` in `frmGames_Load` for displaying tracked apps and times. Initialized and started `trackingTimer` with 1-second interval, linked to `UpdateTrackedApps`. Implemented `UpdateTrackedApps` to manage tracking logic and update UI.

--- MainForms/frmGames.vb
+++ MainForms/frmGames.vb
@@ -1,16 +1,32 @@
 ﻿Imports System.IO
 Imports System.Drawing
 Imports MySql.Data.MySqlClient
+Imports System.Diagnostics
 
 Public Class frmGames
+
+    Private trackingTimer As Timer
+    Private trackedApps As New Dictionary(Of String, DateTime)
+    Private totalElapsedTimes As New Dictionary(Of String, TimeSpan)
 
     Private Sub frmGames_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Configure ListView
         ListViewApps.View = View.LargeIcon
         ListViewApps.LargeImageList = ImageList
 
+        ' Configure ListViewTrackApp
+        ListViewTrackApp.View = View.Details
+        ListViewTrackApp.Columns.Add("Application", 150)
+        ListViewTrackApp.Columns.Add("Elapsed Time", 100)
+
         ' Load saved game paths from the database
         LoadGamePaths()
+
+        ' Initialize and start the tracking timer
+        trackingTimer = New Timer()
+        AddHandler trackingTimer.Tick, AddressOf UpdateTrackedApps
+        trackingTimer.Interval = 1000 ' 1 second
+        trackingTimer.Start()
     End Sub
 
     Private Sub btnAddFiles_Click(sender As Object, e As EventArgs) Handles btnAddFiles.Click
@@ -115,5 +131,53 @@ Public Class frmGames
                 command.ExecuteNonQuery()
             End Using
         End Using
+    End Sub
+
+    Private Sub UpdateTrackedApps(sender As Object, e As EventArgs)
+        For Each item As ListViewItem In ListViewApps.Items
+            Dim exePath As String = item.Tag.ToString()
+            Dim processName As String = Path.GetFileNameWithoutExtension(exePath)
+
+            Dim runningProcess As Process = Process.GetProcessesByName(processName).FirstOrDefault()
+            If runningProcess IsNot Nothing Then
+                If Not trackedApps.ContainsKey(processName) Then
+                    trackedApps(processName) = DateTime.Now
+                End If
+
+                Dim elapsedTime As TimeSpan = DateTime.Now - trackedApps(processName)
+                If totalElapsedTimes.ContainsKey(processName) Then
+                    totalElapsedTimes(processName) += elapsedTime
+                Else
+                    totalElapsedTimes(processName) = elapsedTime
+                End If
+
+                Dim trackItem As ListViewItem = ListViewTrackApp.Items.Cast(Of ListViewItem)().FirstOrDefault(Function(i) i.Text = processName)
+                If trackItem Is Nothing Then
+                    trackItem = New ListViewItem(processName)
+                    trackItem.SubItems.Add(totalElapsedTimes(processName).ToString("hh\:mm\:ss"))
+                    ListViewTrackApp.Items.Add(trackItem)
+                Else
+                    trackItem.SubItems(1).Text = totalElapsedTimes(processName).ToString("hh\:mm\:ss")
+                End If
+
+                trackedApps(processName) = DateTime.Now
+            Else
+                If trackedApps.ContainsKey(processName) Then
+                    Dim elapsedTime As TimeSpan = DateTime.Now - trackedApps(processName)
+                    If totalElapsedTimes.ContainsKey(processName) Then
+                        totalElapsedTimes(processName) += elapsedTime
+                    Else
+                        totalElapsedTimes(processName) = elapsedTime
+                    End If
+
+                    Dim trackItem As ListViewItem = ListViewTrackApp.Items.Cast(Of ListViewItem)().FirstOrDefault(Function(i) i.Text = processName)
+                    If trackItem IsNot Nothing Then
+                        trackItem.SubItems(1).Text = totalElapsedTimes(processName).ToString("hh\:mm\:ss")
+                    End If
+
+                    trackedApps.Remove(processName)
+                End If
+            End If
+        Next
     End Sub
 End Class
