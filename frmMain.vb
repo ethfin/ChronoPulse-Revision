d8da065bf2e471ab6470cf4f7b680fb6d6230419
ethfin <christophergalano13@gmail.com>
2025-02-20 14:55:15 +0800

    Disable games form loading in btnGames_Click
    Commented out the line that loads the frmGames form in the
    btnGames_Click event handler of the frmMain class.

--- frmMain.vb
+++ frmMain.vb
@@ -200,6 +200,6 @@ Public Class frmMain
     End Sub
 
     Private Sub btnGames_Click(sender As Object, e As EventArgs) Handles btnGames.Click
-        LoadForm(New frmGames)
+        'LoadForm(New frmGames)
     End Sub
 End Class
