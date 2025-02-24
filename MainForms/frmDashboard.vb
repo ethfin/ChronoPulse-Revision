2746ae089f9450fe08fb50de39effcca5c504f2e
ethfin <christophergalano13@gmail.com>
2025-02-24 19:42:33 +0800

    Enhance dgExpenses DataGridView appearance and behavior
    Added properties to the dgExpenses DataGridView in frmDashboard
    to improve its styling and functionality. This includes setting
    the data source, disabling row addition, customizing colors,
    fonts, and enabling auto-sizing of columns for better readability.

--- MainForms/frmDashboard.vb
+++ MainForms/frmDashboard.vb
@@ -17,6 +17,21 @@ Public Class frmDashboard
         End Using
 
         dgExpenses.DataSource = dt
+        dgExpenses.DataSource = dt
+        dgExpenses.AllowUserToAddRows = False
+        dgExpenses.BackgroundColor = Color.White
+        dgExpenses.BorderStyle = BorderStyle.None
+        dgExpenses.DefaultCellStyle.SelectionBackColor = Color.FromArgb(245, 245, 245)
+        dgExpenses.DefaultCellStyle.SelectionForeColor = Color.Black
+        dgExpenses.EnableHeadersVisualStyles = False
+        dgExpenses.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None
+        dgExpenses.ColumnHeadersDefaultCellStyle.BackColor = Color.White
+        dgExpenses.ColumnHeadersDefaultCellStyle.ForeColor = Color.Black
+        dgExpenses.ColumnHeadersDefaultCellStyle.Font = New Font("Segoe UI", 10, FontStyle.Regular)
+        dgExpenses.DefaultCellStyle.Font = New Font("Segoe UI", 9.5F, FontStyle.Regular)
+        dgExpenses.RowHeadersVisible = False
+        dgExpenses.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+        dgExpenses.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(250, 250, 250)
         dgExpenses.AllowUserToAddRows = False
         dgExpenses.CellBorderStyle = DataGridViewCellBorderStyle.None
     End Sub
