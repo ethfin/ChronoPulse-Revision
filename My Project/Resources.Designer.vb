33081801b9ce07dd57d0669a60133fbc3b85c31b
ethfin <christophergalano13@gmail.com>
2025-02-28 22:03:01 +0800

    Update login form font to Pixelify Sans
    Changed various controls in `frmLogin.Designer.vb` to use "Pixelify Sans" font, enhancing visual consistency. Adjusted control sizes for better layout. Added new font resource `PixelifySans-VariableFont_wght.ttf` and updated `Resources.Designer.vb` and `Resources.resx` to include it, improving overall user experience.

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -271,6 +271,16 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized resource of type System.Byte[].
+        '''</summary>
+        Friend ReadOnly Property PixelifySans_VariableFont_wght() As Byte()
+            Get
+                Dim obj As Object = ResourceManager.GetObject("PixelifySans_VariableFont_wght", resourceCulture)
+                Return CType(obj,Byte())
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
         Friend ReadOnly Property user() As System.Drawing.Bitmap
