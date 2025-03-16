3b62cc43a966c8b38a072298dbb9703b7dea5706
ethfin <christophergalano13@gmail.com>
2025-03-16 21:21:48 +0800

    Refactor frmOCR layout and button properties
    Updated button controls in frmOCR.Designer.vb, including
    reordering and redefining properties for btnOpenFile,
    btnAnalyze, btnUploadData, and added btnClearAIHistory.
    Adjusted rtbAIResponse size from 450 to 384 pixels and
    modified button layout within Guna2Panel1. Set btnAnalyze
    visibility to false to improve UI functionality.

--- MainForms/frmOCR.Designer.vb
+++ MainForms/frmOCR.Designer.vb
@@ -25,10 +25,10 @@ Partial Class frmOCR
         Me.openFileDialog = New System.Windows.Forms.OpenFileDialog()
         Me.rtbAIResponse = New System.Windows.Forms.RichTextBox()
         Me.Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
-        Me.btnOpenFile = New Guna.UI2.WinForms.Guna2Button()
-        Me.btnAnalyze = New Guna.UI2.WinForms.Guna2Button()
-        Me.btnUploadData = New Guna.UI2.WinForms.Guna2Button()
         Me.btnClearAIHistory = New Guna.UI2.WinForms.Guna2Button()
+        Me.btnUploadData = New Guna.UI2.WinForms.Guna2Button()
+        Me.btnAnalyze = New Guna.UI2.WinForms.Guna2Button()
+        Me.btnOpenFile = New Guna.UI2.WinForms.Guna2Button()
         Me.Guna2Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -44,7 +44,7 @@ Partial Class frmOCR
         Me.rtbAIResponse.ForeColor = System.Drawing.Color.FromArgb(CType(CType(125, Byte), Integer), CType(CType(137, Byte), Integer), CType(CType(149, Byte), Integer))
         Me.rtbAIResponse.Location = New System.Drawing.Point(0, 0)
         Me.rtbAIResponse.Name = "rtbAIResponse"
-        Me.rtbAIResponse.Size = New System.Drawing.Size(800, 450)
+        Me.rtbAIResponse.Size = New System.Drawing.Size(800, 384)
         Me.rtbAIResponse.TabIndex = 2
         Me.rtbAIResponse.Text = ""
         '
@@ -61,24 +61,43 @@ Partial Class frmOCR
         Me.Guna2Panel1.Size = New System.Drawing.Size(800, 66)
         Me.Guna2Panel1.TabIndex = 6
         '
-        'btnOpenFile
+        'btnClearAIHistory
         '
-        Me.btnOpenFile.Anchor = System.Windows.Forms.AnchorStyles.Left
-        Me.btnOpenFile.Animated = True
-        Me.btnOpenFile.BorderRadius = 15
-        Me.btnOpenFile.DisabledState.BorderColor = System.Drawing.Color.DarkGray
-        Me.btnOpenFile.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
-        Me.btnOpenFile.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
-        Me.btnOpenFile.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
-        Me.btnOpenFile.FillColor = System.Drawing.Color.FromArgb(CType(CType(173, Byte), Integer), CType(CType(181, Byte), Integer), CType(CType(211, Byte), Integer))
-        Me.btnOpenFile.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold)
-        Me.btnOpenFile.ForeColor = System.Drawing.Color.Black
-        Me.btnOpenFile.IndicateFocus = True
-        Me.btnOpenFile.Location = New System.Drawing.Point(12, 19)
-        Me.btnOpenFile.Name = "btnOpenFile"
-        Me.btnOpenFile.Size = New System.Drawing.Size(112, 35)
-        Me.btnOpenFile.TabIndex = 12
-        Me.btnOpenFile.Text = "Scan"
+        Me.btnClearAIHistory.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.btnClearAIHistory.Animated = True
+        Me.btnClearAIHistory.BorderRadius = 15
+        Me.btnClearAIHistory.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnClearAIHistory.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btnClearAIHistory.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btnClearAIHistory.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btnClearAIHistory.FillColor = System.Drawing.Color.FromArgb(CType(CType(173, Byte), Integer), CType(CType(181, Byte), Integer), CType(CType(211, Byte), Integer))
+        Me.btnClearAIHistory.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnClearAIHistory.ForeColor = System.Drawing.Color.Black
+        Me.btnClearAIHistory.IndicateFocus = True
+        Me.btnClearAIHistory.Location = New System.Drawing.Point(676, 19)
+        Me.btnClearAIHistory.Name = "btnClearAIHistory"
+        Me.btnClearAIHistory.Size = New System.Drawing.Size(112, 35)
+        Me.btnClearAIHistory.TabIndex = 15
+        Me.btnClearAIHistory.Text = "Clear"
+        '
+        'btnUploadData
+        '
+        Me.btnUploadData.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.btnUploadData.Animated = True
+        Me.btnUploadData.BorderRadius = 15
+        Me.btnUploadData.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnUploadData.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btnUploadData.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btnUploadData.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btnUploadData.FillColor = System.Drawing.Color.FromArgb(CType(CType(173, Byte), Integer), CType(CType(181, Byte), Integer), CType(CType(211, Byte), Integer))
+        Me.btnUploadData.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnUploadData.ForeColor = System.Drawing.Color.Black
+        Me.btnUploadData.IndicateFocus = True
+        Me.btnUploadData.Location = New System.Drawing.Point(558, 19)
+        Me.btnUploadData.Name = "btnUploadData"
+        Me.btnUploadData.Size = New System.Drawing.Size(112, 35)
+        Me.btnUploadData.TabIndex = 14
+        Me.btnUploadData.Text = "Save"
         '
         'btnAnalyze
         '
@@ -100,51 +119,32 @@ Partial Class frmOCR
         Me.btnAnalyze.Text = "Analyze"
         Me.btnAnalyze.Visible = False
         '
-        'btnUploadData
+        'btnOpenFile
         '
-        Me.btnUploadData.Anchor = System.Windows.Forms.AnchorStyles.Right
-        Me.btnUploadData.Animated = True
-        Me.btnUploadData.BorderRadius = 15
-        Me.btnUploadData.DisabledState.BorderColor = System.Drawing.Color.DarkGray
-        Me.btnUploadData.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
-        Me.btnUploadData.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
-        Me.btnUploadData.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
-        Me.btnUploadData.FillColor = System.Drawing.Color.FromArgb(CType(CType(173, Byte), Integer), CType(CType(181, Byte), Integer), CType(CType(211, Byte), Integer))
-        Me.btnUploadData.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold)
-        Me.btnUploadData.ForeColor = System.Drawing.Color.Black
-        Me.btnUploadData.IndicateFocus = True
-        Me.btnUploadData.Location = New System.Drawing.Point(558, 19)
-        Me.btnUploadData.Name = "btnUploadData"
-        Me.btnUploadData.Size = New System.Drawing.Size(112, 35)
-        Me.btnUploadData.TabIndex = 14
-        Me.btnUploadData.Text = "Save"
-        '
-        'btnClearAIHistory
-        '
-        Me.btnClearAIHistory.Anchor = System.Windows.Forms.AnchorStyles.Right
-        Me.btnClearAIHistory.Animated = True
-        Me.btnClearAIHistory.BorderRadius = 15
-        Me.btnClearAIHistory.DisabledState.BorderColor = System.Drawing.Color.DarkGray
-        Me.btnClearAIHistory.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
-        Me.btnClearAIHistory.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
-        Me.btnClearAIHistory.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
-        Me.btnClearAIHistory.FillColor = System.Drawing.Color.FromArgb(CType(CType(173, Byte), Integer), CType(CType(181, Byte), Integer), CType(CType(211, Byte), Integer))
-        Me.btnClearAIHistory.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold)
-        Me.btnClearAIHistory.ForeColor = System.Drawing.Color.Black
-        Me.btnClearAIHistory.IndicateFocus = True
-        Me.btnClearAIHistory.Location = New System.Drawing.Point(676, 19)
-        Me.btnClearAIHistory.Name = "btnClearAIHistory"
-        Me.btnClearAIHistory.Size = New System.Drawing.Size(112, 35)
-        Me.btnClearAIHistory.TabIndex = 15
-        Me.btnClearAIHistory.Text = "Clear"
+        Me.btnOpenFile.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.btnOpenFile.Animated = True
+        Me.btnOpenFile.BorderRadius = 15
+        Me.btnOpenFile.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnOpenFile.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btnOpenFile.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btnOpenFile.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btnOpenFile.FillColor = System.Drawing.Color.FromArgb(CType(CType(173, Byte), Integer), CType(CType(181, Byte), Integer), CType(CType(211, Byte), Integer))
+        Me.btnOpenFile.Font = New System.Drawing.Font("Century Gothic", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnOpenFile.ForeColor = System.Drawing.Color.Black
+        Me.btnOpenFile.IndicateFocus = True
+        Me.btnOpenFile.Location = New System.Drawing.Point(12, 19)
+        Me.btnOpenFile.Name = "btnOpenFile"
+        Me.btnOpenFile.Size = New System.Drawing.Size(112, 35)
+        Me.btnOpenFile.TabIndex = 12
+        Me.btnOpenFile.Text = "Scan"
         '
         'frmOCR
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Guna2Panel1)
         Me.Controls.Add(Me.rtbAIResponse)
+        Me.Controls.Add(Me.Guna2Panel1)
         Me.Name = "frmOCR"
         Me.Text = "frmOCR"
         Me.Guna2Panel1.ResumeLayout(False)
